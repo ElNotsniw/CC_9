15ccc2868ce1e50ff5468bdcbf0530240ac6f9d7
ElNotsniw <winstonjosh010604@gmail.com>
2025-02-23 16:00:46 -0500

    Task 5 - Implemented Promotion System

--- cc_9.js
+++ cc_9.js
@@ -23,8 +23,8 @@ class Employee {
 
 // Creating an employee and getting their annual salary and console-logging it
 
-const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000)          // Output: Employee Name: Alice Johnson, ID: 101, Department: Sales, Salary: 5000
-console.log(`Employee Name: ${emp1.getdetails()}`);
+const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000)          // Output: Employee: Alice Johnson, ID: 101, Department: Sales, Salary: 5000
+console.log(`Employee: ${emp1.getdetails()}`);
 
 console.log(`Annual Salary: $${emp1.calculateAnnualSalary()}`);         // Output: Annual Salary: $60000
 
@@ -46,7 +46,7 @@ class Manager extends Employee {
 // Override the first getdetails () method from Task 1 and create a new method to calculate bonus
 
     getdetails() {
-        return `${super.getdetails()}, Team Size: ${this.teamSize}`;
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: ${this.salary} Team Size: ${this.teamSize}`;
     }
 
     calculateBonus() {
@@ -64,7 +64,7 @@ class Manager extends Employee {
 // Create a Manager that inherits properties from the Employee class and adding a new property: Team Size, and calculating bonus
 
 const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
-console.log(`Manager: ${mgr1.getdetails()}`);                             // Output: Employee Name: John Smith, ID: 201, Department: IT, Salary: 8000, Team Size: 5
+console.log(mgr1.getdetails());                             // Output: Employee Name: John Smith, ID: 201, Department: IT, Salary: 8000, Team Size: 5
 
 console.log(`Bonus: $${mgr1.calculateBonus()}`);            // Output: Bonus: $9600
 
@@ -89,14 +89,9 @@ class Company {
 
     listEmployee() {
         this.employees.forEach(employee => {
-            if (employee.name === "Alice Johnson") {
-                return console.log(`Employee: ${employee.getdetails()}`);
-            }
-            else
-            return console.log(`Manager: ${employee.getdetails()}`)
-        });
+            console.log(employee.getdetails())
+        })
     }
-
 
 // Task 4 - Implementing a Payroll System
 
@@ -105,15 +100,30 @@ class Company {
     calculateTotalPayroll () {
         return this.employees.reduce((total, employee) => total + employee.calculateAnnualSalary(), 0)
         }
+
+
+
+    // Task 5 - Implementing Promotions
+
+    // Creating a method promoteToManager to make an Employee to a Manager
+
+    promoteToManager(employee, teamSize) {
+        const promotedManager = new Manager(employee.name, employee.id, employee.department, employee.salary, teamSize)
+
+        const index = this.employees.indexOf(employee)
+        if (index !== -1) {
+            this.employees[index] = promotedManager
+            }
+        }
     }
 
-
-
-
     
+
+
+
 // (Task 3): Naming the company and then console-logging the employees to associate with the company 
 
-const company = new Company ("TechCorp:");
+const company = new Company ("TechCorp");
 
 company.addEmployee(emp1);                      // Output: Employee: Alice Johnson, ID: 101, Department: Sales, Salary: 5000
 company.addEmployee(mgr1);                      // Output: Manager: John Smith, ID: 201, Department: IT, Salary: 8000, Team Size: 5
@@ -126,3 +136,12 @@ company.listEmployee();                         // Shows the two employees above
 // (Task 4): Console-logging the total payroll
 
 console.log(`Total Payroll: $${company.calculateTotalPayroll()}`)           // Output: Total Payroll: $165600
+
+
+
+
+// (Task 5) - Console-logging emp1 so that that employee becomes a Manager and have a team size of 3
+
+company.promoteToManager(emp1, 3)
+company.listEmployee()              // Output: Manager: Alice Johnson, ID: 101, Department: Sales, Salary: 5000, Team Size: 3
+                                    // Output: Manager: John Smith, ID: 201, Department: IT, Salary: 8000, Team Size: 5
